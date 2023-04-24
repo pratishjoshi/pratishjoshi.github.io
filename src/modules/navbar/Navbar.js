8080ae27d9a9e6320c89e869d93f52895821ee42
ace <pratish.joshi@krafters.dev>
2023-04-24 23:48:18 +0545

    Responsive design navbar

--- src/modules/navbar/Navbar.js
+++ src/modules/navbar/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
       <div className="container mx-auto">
         <ul
           id="navbar-list"
-          className="list-none flex gap-12 h-14 items-center"
+          className="list-none flex gap-4 h-14 lg:gap-12 lg:justify-start justify-center items-center overflow-x-hidden text-xs md:text-lg"
         >
           {navbarList.map((data, key) => (
             <a
